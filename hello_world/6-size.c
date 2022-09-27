95aaee57621c9e5e88e0e9c87c7e54e53734c040
Sapitorico <renzoyaques@gmail.com>
2022-09-27 14:43:14 -0700

    fix menssage

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - 
+ * main - print bytes variables
  *
  * Return: Always 0 (Success)
  */
@@ -14,10 +14,9 @@ int main(void)
 	float simple_decimal;
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(character));
-	printf("Size of a char: %zu byte(s)\n", sizeof(inter));
-	printf("Size of a char: %zu byte(s)\n", sizeof(long_inter));
-	printf("Size of a char: %zu byte(s)\n", sizeof(inter_64));
-	printf("Size of a char: %zu byte(s)\n", sizeof(simple_decimal));
-
-	return(0);
+	printf("Size of an int: %zu byte(s)\n", sizeof(inter));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long_inter));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(inter_64));
+	printf("Size of a float %zu byte(s)\n", sizeof(simple_decimal));
+	return (0);
 }
