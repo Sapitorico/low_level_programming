162cc1f1d1fefffd14b5b7d974de2d7ecda52954
Sapitorico <renzoyaques@gmail.com>
2022-09-27 14:38:14 -0700

    print count bytes

---
+++ hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char character;
+	int inter;
+	long int long_inter;
+	long long int inter_64;
+	float simple_decimal;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(character));
+	printf("Size of a char: %zu byte(s)\n", sizeof(inter));
+	printf("Size of a char: %zu byte(s)\n", sizeof(long_inter));
+	printf("Size of a char: %zu byte(s)\n", sizeof(inter_64));
+	printf("Size of a char: %zu byte(s)\n", sizeof(simple_decimal));
+
+	return(0);
+}
