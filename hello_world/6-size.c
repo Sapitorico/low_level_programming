02bf0df7a383ae55d1f761b34516cf034607837f
Sapitorico <renzoyaques@gmail.com>
2022-09-27 14:45:06 -0700

    :

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -14,9 +14,9 @@ int main(void)
 	float simple_decimal;
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(character));
-	printf("Size of a int: %zu byte(s)\n", sizeof(inter));
+	printf("Size of an int: %zu byte(s)\n", sizeof(inter));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long_inter));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(inter_64));
-	printf("Size of a float %zu byte(s)\n", sizeof(simple_decimal));
+	printf("Size of a float: %zu byte(s)\n", sizeof(simple_decimal));
 	return (0);
 }
