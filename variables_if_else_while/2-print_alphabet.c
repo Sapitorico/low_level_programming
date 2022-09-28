4c868a265152e36afc3c5695c2b56c9202ca89f5
Sapitorico <renzoyaques@gmail.com>
2022-09-28 04:45:44 -0700

    print alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int chars;
+
+	for (chars = 'a'; chars <= 'z'; chars++);
+		putchar(chars);
+	putchar('\n');
+	return(0);
+}
