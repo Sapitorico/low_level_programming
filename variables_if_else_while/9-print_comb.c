6b6eed094ff5ef0c5c1d5b20868cccf71863f064
Sapitorico <renzoyaques@gmail.com>
2022-09-28 14:37:28 -0700

    pinter a charspe

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,14 +7,14 @@
 int main(void)
 {
 	int num = 0;
+	char *sep = ", ";
 
 	for (; num <= 9; num++)
 	{
 		putchar((num % 10) + '0');
 		if (num != 9)
 		{
-			putchar(',');
-			putchar(' ');
+			putchar(*sep);
 		}
 	}
 	putchar('\n');
