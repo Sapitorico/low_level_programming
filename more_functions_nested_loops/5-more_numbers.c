66f0f485756c03d8a8bbfb028507f0b0e522436b
Sapitorico <renzoyaques@gmail.com>
2022-10-01 17:33:40 -0700

    10 times the numbers, from 0 to 14

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ */
+void more_numbers(void)
+{
+	int time;
+	int num;
+
+	for (time = 0; time < 10; time++)
+	{
+		for (num = 0; num < 15; num++)
+		{
+			_putchar(num % 10 + '0');
+			if (num > 9)
+			{
+				_putchar(num / 10 + '0');
+				_putchar(num % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
