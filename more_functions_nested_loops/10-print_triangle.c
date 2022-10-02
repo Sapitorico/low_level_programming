f3c227afdfacb79fe14d3ec0a6cb0d1894a86f48
Sapitorico <renzoyaques@gmail.com>
2022-10-02 05:20:47 -0700

    prints a triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle
+ * @size: is the size of the triangle
+ */
+void print_triangle(int size)
+{
+	int x, y;
+
+	if (size > 0)
+	{
+		for (x = 0; x <= size; x++)
+		{
+			for (y = 0; y <= size; y++)
+			{
+				if (y != size && y <= size -x)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(35);
+				}
+			}
+			_putchar('\n');
+
+		}
+	}
+	else
+		_putchar('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -25,4 +25,6 @@ void print_diagonal(int n);
 
 void print_square(int size);
 
+void print_triangle(int size);
+
 #endif /*main.h*/
