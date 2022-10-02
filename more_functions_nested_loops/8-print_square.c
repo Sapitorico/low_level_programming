17bafdf95d75687cb878a65c5ed7fc896bf8deb9
Sapitorico <renzoyaques@gmail.com>
2022-10-01 19:38:08 -0700

    prints a square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - prints a square
+ * @size: is the size of the square
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	if (size > 0)
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
