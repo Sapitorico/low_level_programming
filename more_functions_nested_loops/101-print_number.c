eb5b60da6dc36b24299c85656411786882d568f6
Sapitorico <renzoyaques@gmail.com>
2022-10-05 13:55:42 -0700

    fix?

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -5,12 +5,26 @@
  */
 void print_number(int n)
 {
-	if (n < 0)
+	if(n / 10 != 0)
 	{
-		n = -n;
+		_putchar(n % 10);
+		print_number((n-n % 10) / 10);
+		if ( n > 0 )
+		{
+			_putchar(n % 10 + '0');
+		}
+		else
+		{
+			_putchar(-n % 10 + '0');
+		}
 	}
-	if (n / 10)
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
-
+	else if((n / 10 == 0) && (n % 10 != 0) && (n > 0))
+	{
+		_putchar(n % 10 + '0');
+	}
+	else if((n / 10 == 0) && (n % 10 != 0) && (n <= 0))
+	{
+		_putchar('-');
+		_putchar(-n % 10 + '0');
+	}
 }
