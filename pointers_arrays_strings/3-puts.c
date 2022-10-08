4349e9c5d64bdce2a601519669abe5bd16b3593e
Sapitorico <renzoyaques@gmail.com>
2022-10-08 14:44:06 -0700

    print a string

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _puts
- * @str: pinter to string
+ * _puts - prints a string
+ * @str: pointer to the address of the first position of the string
  */
 void _puts(char *str)
 {
-	for (;*str;i++)
+	for (; *str; str++)
 		_putchar(*str);
 	_putchar('\n');
 }
