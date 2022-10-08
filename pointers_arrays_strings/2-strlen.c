7fb7a6c1778f43a76b79c1f180c91b7e107da34b
Sapitorico <renzoyaques@gmail.com>
2022-10-08 14:30:56 -0700

    fix betty

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -2,11 +2,13 @@
 /**
  * _strlen - returns the length of a string
  * @s: pinter to addres os strign
+ * Return: string length
  */
 int _strlen(char *s)
 {
 	int count = 0;
 
-	for (;s[count] != '\0'; count++);
+	for (; s[count] != '\0'; count++)
+		;
 	return (count);
 }
