def1ae740cdb020b3d8c5ef95d21c4381153b9aa
Sapitorico <renzoyaques@gmail.com>
2022-10-10 14:10:44 -0700

    print string in reerse

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int temp;
+	int len = n -1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[len - i];
+		a[len - i] = temp;
+	}
+}
