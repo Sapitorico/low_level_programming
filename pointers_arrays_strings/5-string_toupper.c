fe5446f544074a9db190f0560a2026c89d5ed8b2
Sapitorico <renzoyaques@gmail.com>
2022-10-10 14:38:52 -0700

    change al loworcase uppercase

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
