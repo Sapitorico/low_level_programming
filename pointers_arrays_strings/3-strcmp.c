14ddee75ab339b5ec7a395e457488b83d37c2ede
Sapitorico <renzoyaques@gmail.com>
2022-10-11 16:57:38 -0700

    fix

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -5,12 +5,8 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
-	{
+	for (; s1[i] && s2[i]; i++)
 		if (s1[i] != s2[i])
-		{
 			return (s1[i] - s2[i]);
-		}
-	}
 	return (0);
 }
