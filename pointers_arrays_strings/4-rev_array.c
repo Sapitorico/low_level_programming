0ef27b5ddfaafacaee422692d9a0462f68344290
Sapitorico <renzoyaques@gmail.com>
2022-10-11 18:07:53 -0700

    capitalizes all words of a string.

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -5,7 +5,7 @@ void reverse_array(int *a, int n)
 {
 	int i;
 	int temp;
-	int len = n -1;
+	int len = n - 1;
 
 	for (i = 0; i < len / 2; i++)
 	{
--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,21 +1,22 @@
 #include "main.h"
 /**
  */
-char *cap_string(char *str)
-{
-	char *ch = string_toupper(str);
-
-}
-char *string_toupper(char *str)
+char *cap_string(char *s)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	i = 0;
+	while (s[i] != '\0')
 	{
-		if (str[i] >= 97 && str[i] <= 122)
+		while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?' || s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
 		{
-			str[i] = str[i] - 32;
+			i++;
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] = s[i] - 32;
+			}
 		}
+		i++;
 	}
-	return (str);
+	return (s);
 }
