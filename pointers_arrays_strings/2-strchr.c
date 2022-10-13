ec38d2bb523767648ce1b75a9f1ee5348e1c9293
Sapitorico <renzoyaques@gmail.com>
2022-10-13 10:51:33 -0700

    look up a string for a set of bytes

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -4,15 +4,15 @@
  * @s: pointer to the string
  * @c: desired character
  *
- * Return: to the matching position, if equal 0
+ * Return: to the matching position, if null 0
  */
 char *_strchr(char *s, char c)
 {
 	for (; ; s++)
 	{
-		if (*s == c)
+		if (*s == c)	/*search for a match with c*/
 			return (s);
-		if (!*s)
+		if (!*s)	/*if null*/
 			return (0);
 	}
 }
--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,10 @@
 #include "main.h"
 /**
+ * _strspn - gets the length of a prefix substring
+ * @s: pointer to the string
+ * @accept: bytes of the substring
+ *
+ * Return: the bytes of the found substring
  */
 unsigned int _strspn(char *s, char *accept)
 {
@@ -13,6 +18,13 @@ unsigned int _strspn(char *s, char *accept)
 	}
 	return (i);
 }
+/**
+ * _strchr - locates a character in a string
+ * @s: pointer to the string
+ * @c: desired character
+ *
+ * Return: to the matching position, if equal 0
+ */
 char *_strchr(char *s, char c)
 {
 	for (; ; s++)
