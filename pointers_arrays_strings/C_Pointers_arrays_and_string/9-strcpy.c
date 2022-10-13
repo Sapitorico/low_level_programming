4eb976cc4a7c740f3bc882101942cc4b6e2c7a9e
Sapitorico <renzoyaques@gmail.com>
2022-10-13 06:22:48 -0700

    copies a character string by subscribing to its destination character string

--- pointers_arrays_strings/C_Pointers_arrays_and_string/9-strcpy.c
+++ pointers_arrays_strings/C_Pointers_arrays_and_string/9-strcpy.c
@@ -3,7 +3,8 @@ include "main.h"
  * *_strcpy -  function that copies the string
  * @src: pointer to the strign
  * @dest: string copy destination
- * Return: dest
+ *
+ * Return: returns copied string
  */
 char *_strcpy(char *dest, char *src)
 {
