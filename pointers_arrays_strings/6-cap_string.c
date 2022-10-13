fc2850e7dab198138db6a4dc78192ada6a814af9
Sapitorico <renzoyaques@gmail.com>
2022-10-13 08:07:25 -0700

    fix loop reinicie

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -10,16 +10,17 @@ char *cap_string(char *s)
 	char separators [] ={32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	if (s[i] >= 97 && s[i] <= 122)
-		s[i] -= 32;
-	for (; s[i]; i++)
+		s[i] = (s[i] - 32);
+	for (i = 0; s[i]; i++)
 	{
-		for (; separators[countsep]; countsep++)
+		for (countsep = 0; separators[countsep]; countsep++)
 		{
 			if (s[i] == separators[countsep])
 			{
 				if (s[i + 1] >= 97 && s[i + 1] <= 122)
 				{
 					s[i + 1] -= 32;
+					break;
 				}
 			}
 		}
