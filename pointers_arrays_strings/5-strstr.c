9dcb3ca6c0803d34be9189d3120ccc3980a619a5
Sapitorico <renzoyaques@gmail.com>
2022-10-13 11:40:48 -0700

    locates a substring.

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,30 +1,50 @@
 #include "main.h"
+int _strncmp(char *s1, char *s2, int n);
 /**
+ * _strstr- locates a substring
+ * @haystack: pointer to the string
+ * @needle: pointer to the substring
+ *
+ * Return: position of the found substring, NULL substring is not found.
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i = 0;
-	char b = 0;
+	char *aux = haystack;
 
-	while (haystack[i])
+	for (; *aux; aux++)
 	{
-		while (needle[b] && (haystack[i] == needle[0]))
-		{
-			if (haystack[i + b] == needle[b])
-			{
-				b++;
-			}
-			else
-				break;
-		
-		}
-		if (needle[b])
-		{
-			i++;
-			b = 0;
-		}
-		else
-			return (haystack + i);
+		if (_strncmp(aux, needle, _strlen(needle)) == 0)
+			return (aux);
 	}
+	return (NULL);
+}
+/**
+ * _strncmp - compares two strings
+ * @s1: pointer to the compared string
+ * @s2: pointer to comparison strin
+ * @n: s2 elements
+ *
+ * Return: difference of characters in their ASCII numeric value or 0 if equal
+ */
+int _strncmp(char *s1, char *s2, int n)
+{
+	int i = 0;
+
+	for (; i < n && s1[i] && s2[i]; i++)
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 	return (0);
+}
+/**
+ * _strlen - returns the length of a string
+ * @s: pinter to addres os strign
+ * Return: all elements of the stirng
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	for (; s[count]; count++)
+		;
+	return (count);
 }
