6c235361b79079c78a4ce2968bee8946d41ea92a
Sapitorico <renzoyaques@gmail.com>
2022-10-13 07:53:23 -0700

    fic positons tr

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -6,20 +6,20 @@
 char *cap_string(char *s)
 {
 	int i = 0;
-	int count = 0;
-	char separator [] ={32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int countsep = 0;
+	char separators [] ={32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
+	if (s[i] >= 97 && s[i] <= 122)
+		s[i] -= - 32;
 	for (; s[i]; i++)
 	{
-		if (s[0] >= 97 && s[0] <= 122)
-			s[0] = (s[0] - 32);
 		for (; separator[count]; count++)
 		{
-			if (s[i + 1] == separator[count])
+			if (s[i] == separator[count])
 			{
 				if (s[i + 1] >= 97 && s[i + 1] <= 122)
 				{
-					s[i + 1] = (s[i + 1] - 32);
+					s[i + 1] -= - 32;
 				}
 			}
 		}
