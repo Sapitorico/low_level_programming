78d4dbeb33c1612a68fc1ea3654ceca0e9f66f25
Sapitorico <renzoyaques@gmail.com>
2022-10-16 14:14:07 -0700

    print a string using recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -7,8 +7,8 @@ void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar(*s);	/*print*/
+		_puts_recursion(s + 1);	/*advances in the stack*/
 	}
 	else
 		_putchar(10);
