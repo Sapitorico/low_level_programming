e2f329354fb19847200d362d89ad2ee96ba31db2
Sapitorico <renzoyaques@gmail.com>
2022-10-20 12:13:55 -0700

    fix

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -22,8 +22,8 @@ int **alloc_grid(int width, int height)
 		arr[i] = malloc(width * sizeof(int));
 		if (!arr[i])
 		{
-			for (; j < i; j--)
-				free(arr[j]);
+			for (--i; i >= 0; i--)
+				free(arr[i]);
 			free(arr);
 			return (arr);
 		}
