8bb989c738c5279a7e225efb9c76b221daac1bdf
Sapitorico <renzoyaques@gmail.com>
2022-10-22 07:40:36 -0700

    The memory is set to zero test 3

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,4 +1,5 @@
 #include "main.h"
+char *_memset(char *s, char b, unsigned int n);
 /**
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
@@ -10,5 +11,22 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	arrl = malloc(nmemb * size);
 	if (!arrl)
 		return (NULL);
+	_memset(arrl, 0, size);
 	return (arrl);
+}
+/**
+ * _memset - fills memory with a constant byte
+ * @s: pointer to the array address
+ * @b: is the added constant character or bytes
+ * @n: the constant number of bytes b
+ *
+ * Return: returns the initial position of the array
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
 }
