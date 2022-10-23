b31816a92fb2b54df0b61a6f2c7e63498ef828b1
Sapitorico <renzoyaques@gmail.com>
2022-10-23 12:49:40 -0700

    frees a 2 dimensional

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * free_grid - frees an array
+ * free_grid - frees an array 2D
  * @grid: double pointer to two-dimensional array
  * @height: height of the array
  */
@@ -8,7 +8,7 @@ void free_grid(int **grid, int height)
 {
 	int i = 0;
 
-	for (; i < height ; i++)
+	for (; i < height ; i++)	/*I run through each grid block and apply a free for each block assigned to it*/
 		free(grid[i]);
-	free(grid);
+	free(grid);	/*I release the principal*/
 }
