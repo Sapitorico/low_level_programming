b8bd1e2f3cea11cff65370ed580249c6f4588d1c
Sapitorico <renzoyaques@gmail.com>
2022-10-24 12:08:41 -0700

    recreates the malloc function that allocates a memory space and returns an empty pointer to this new block

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,9 @@
 #include "main.h"
 /**
- * malloc_checked
+ * malloc_checked - allocates a specific amount of memory space in bytes
+ * @b: memory block size positive integer
+ *
+ * Return: empty pointer to the first byte of the allocated block
  */
 void *malloc_checked(unsigned int b)
 {
