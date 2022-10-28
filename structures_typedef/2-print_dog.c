f78fbcb99c53246fa29723b7bcd23645a3e3eb95
Sapitorico <renzoyaques@gmail.com>
2022-10-28 09:54:17 -0700

    test 1

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,12 +1,12 @@
 #include "dog.h"
 /**
+ * print_dog - prints a struct dog
+ * @d: pointer to the struct dog
  */
 void print_dog(struct dog *d)
 {
 	if (!d)
-	{
-		printf("(nil)");
-	}
+		return;
 	printf("Name: %s\n", d->name);
 	printf("Age: %1f\n", d->age);
 	printf("Owner: %s\n", d->owner);
