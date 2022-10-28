9ba052078c56539a0edd1774e4e03779ed8095de
Sapitorico <renzoyaques@gmail.com>
2022-10-28 16:24:09 -0700

    fix memroy

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include "1-init_dog.c"
 /**
  * new_dog - creates a new struct dog
  * @name: dog name
@@ -12,24 +11,26 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *NEW_dog;
 
-	NEW_dog =  malloc(sizeof(dog_t));
-	if (!NEW_dog)
-		return (NULL);
-	NEW_dog->name = malloc(sizeof(name));
-	if (!NEW_dog->name)
+	NEW_dog =  malloc(sizeof(char) * strlen(name) + 1);
+	if (NEW_dog)
 	{
+		NEW_dog->name = malloc(sizeof(char) * strlen(owner) + 1);
+		if (NEW_dog->name)
+		{
+			NEW_dog->name = strcpy(NEW_dog->name, name);
+			NEW_dog->owner = malloc(sizeof(owner));
+			if (NEW_dog->owner)
+			{
+				NEW_dog->owner = strcpy(NEW_dog->owner, owner);
+				NEW_dog->age = age;
+				return (NEW_dog);
+			}
+			free(NEW_dog->name);
+			free(NEW_dog);
+			return (NULL);
+		}
 		free(NEW_dog);
 		return (NULL);
 	}
-	strcpy(NEW_dog->name, name);
-	NEW_dog->age = age;
-	NEW_dog->owner = malloc(sizeof(owner));
-	if (!NEW_dog->owner)
-	{
-		free(NEW_dog->name);
-		free(NEW_dog);
-		return (NULL);
-	}
-	strcpy(NEW_dog->owner, owner);
-	return (NEW_dog);
+	return (NULL);
 }
