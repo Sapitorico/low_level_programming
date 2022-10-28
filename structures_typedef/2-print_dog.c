e716a60fb273a1b555ba87004bf6a637309ff388
Sapitorico <renzoyaques@gmail.com>
2022-10-28 10:14:36 -0700

    fix case If an element of d is NULL, print (nil)

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -7,6 +7,10 @@ void print_dog(struct dog *d)
 {
 	if (!d)
 		return;
+	if (!d->name)
+		d->name = "(nil)";
+	if (!d->owner)
+		d->owner = "(nil)";
 	printf("Name: %s\n", d->name);
 	printf("Age: %1f\n", d->age);
 	printf("Owner: %s\n", d->owner);
