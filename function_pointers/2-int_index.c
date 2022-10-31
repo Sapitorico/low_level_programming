e1c7e184173b199d7e39f27cca3536526b6ddbf2
Sapitorico <renzoyaques@gmail.com>
2022-10-30 17:05:18 -0700

    fix output case cmp is NULL

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -11,12 +11,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (size <= 0 || !array)
+	if (size <= 0 || !array || !cmp)
 		return (-1);
 	for (; i < size; i++)
-	{
 		if ((cmp)(array[i]))
 			return (i);
-	}
 	return (-1);
 }
