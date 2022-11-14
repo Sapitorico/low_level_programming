a16523f59c6c49fef1db8b06dab3aaab6818e6ee
Sapitorico <renzoyaques@gmail.com>
2022-11-14 10:42:56 -0800

    function that returns the number of elements

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,5 +1,9 @@
 #include "lists.h"
 /**
+ * list_len - counts the number of elements
+ * @h: pointer to the structure
+ *
+ * Return: the number of elements
  */
 size_t list_len(const list_t *h)
 {
