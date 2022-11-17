f214765955b59d0f32bf77dbf9707ad3845a63fa
Sapitorico <renzoyaques@gmail.com>
2022-11-17 11:14:19 -0800

    fix 5-free_listint2.c:10: ERROR: trailing whitespace, 5-free_listint2.c:4: warning: Cannot understand  */

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -1,12 +1,14 @@
 #include "lists.h"
 #include "4-free_listint.c"
 /**
+ * free_listint2 - frees a listint_t list.
+ * @head: pointer no first node
  */
 void free_listint2(listint_t **head)
 {
 	if (head)
 	{
 		free_listint(*head);
-		*head = NULL;	
+		*head = NULL
 	}
 }
