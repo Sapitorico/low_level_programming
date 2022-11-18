26528cc7d0269f2e5336942e60a26f545a9dde32
Sapitorico <renzoyaques@gmail.com>
2022-11-17 17:58:18 -0800

    s

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -5,7 +5,7 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *prev = NULL, *node = *head;
+	listint_t *prev = NULL, *node = NULL;
 	int n = 0;
 
 	if (!head)
