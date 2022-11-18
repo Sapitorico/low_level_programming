03ab4e98576ef7c7cfc4b675039b324f721d3e5b
Sapitorico <renzoyaques@gmail.com>
2022-11-17 16:09:53 -0800

    fix

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -12,10 +12,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	node = add_nodeint(&nth, n);
 	if (!node)
 		return (NULL);
-	if (!idx)
-	{
-		node->next = *head;
+	if (nth == *head)
 		*head = node;
-	}
 	return (node);
 }
