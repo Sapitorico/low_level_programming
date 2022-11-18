e8bf42dfcedcbce0943af9db147b1656332770b6
Sapitorico <renzoyaques@gmail.com>
2022-11-17 16:05:01 -0800

    inserts a new node at a given position.

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,4 @@
-#include "list.h"
-#include "2-add_nodeint.c"
+#include "lists.h"
 #include "7-get_nodeint.c"
 /**
  */
@@ -8,17 +7,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *nth = NULL, *node = NULL;
 
 	if (!head)
-		return (head);
-	nth = get_nodeint_at_index(head, idx);
-	node = add_nodeint(nth, n);
-    if (!node)
-        return (NULL);
-    if (!idx)
-    {
-        node->next = *head;
-        *head = node;
-    }
-    
-    return (node);
-}
+		return (*head);
+	nth = get_nodeint_at_index(*head, idx);
+	node = add_nodeint(&nth, n);
+	if (!node)
+		return (NULL);
+	if (!idx)
+	{
+		node->next = *head;
+		*head = node;
+	}
+	return (node);
 }
