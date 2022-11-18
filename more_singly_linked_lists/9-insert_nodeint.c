fc390dd19d7a9412857ba327528958a417822a26
Sapitorico <renzoyaques@gmail.com>
2022-11-17 16:43:29 -0800

    inserts a new node at a given position

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 #include "7-get_nodeint.c"
 /**
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @head: pointer to the first element
+ * @idx: index
+ * @n: element
+ *
+ * Return: new node
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
@@ -13,19 +19,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	node = get_nodeint_at_index(*head, idx - 1);
 	if (!node)
 		return (NULL);
-	/**nth = node->next;
-	node->next = add_nodeint(&(node->next), n);
-	if (!nth)
-		return (NULL);
-	node->next->next = nth;
-	return (node->next);**/
-	/**nth = malloc(sizeof(listint_t));
-	if (!nth)
-		return (NULL);
-	nth->n = n;
-	nth->next = node->next;
-	node->next = nth;
-	return (new);**/
 	nth = add_nodeint(&(node->next), n);
 	return (nth);
 }
