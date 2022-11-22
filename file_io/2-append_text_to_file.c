7f932d40a0b2b6a24d17a30fb318c761e507a32f
Sapitorico <renzoyaques@gmail.com>
2022-11-22 06:29:00 -0800

    error: ‘texxt_content’ undeclar

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -17,7 +17,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (!text_content)
 		return (1);
-	wr = write(fd, texxt_content, strlen(text_content));
+	wr = write(fd, text_content, strlen(text_content));
 	if (wr == -1)
 		return (-1);
 	close(fd);
