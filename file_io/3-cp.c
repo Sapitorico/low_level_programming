255c8e11e388a8b9866d23c440a8dc7d47f3b04e
Sapitorico <renzoyaques@gmail.com>
2022-11-22 08:08:30 -0800

    fix

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -8,37 +8,44 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	ft = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	ft = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (ft == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	rd = read(fd, buff, BUFF_SIZE);
-	if (rd == -1)
+	while ((rd = read(fd, buff, BUFF_SIZE)))
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		if (rd == -1)
+		{
+			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		wr = write(ft, buff, rd);
+		if (wr == -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+
 	}
-	wr = write(ft, buff, rd);
-	if (wr == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	close(fd);
-	if (fd == -1)
+	if ((close(fd) == -1))
 	{
 		dprintf(2, "Error: Can't close fd %ld\n", fd);
+		exit(100);
+	}
+	if ((close(ft) == -1))
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %ld\n", ft);
 		exit(100);
 	}
 	return (0);
