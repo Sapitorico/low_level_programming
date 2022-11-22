b006787755fb033a188b549cc76e53ce567eca3d
Sapitorico <renzoyaques@gmail.com>
2022-11-22 06:10:43 -0800

    fix if (text_content == NULL)

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	if (!text_content)
-		return (-1);
+		return (1);
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
