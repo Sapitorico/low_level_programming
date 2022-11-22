d8cb2b160f92499a43f3dedeaf824614a7c2a62a
Sapitorico <renzoyaques@gmail.com>
2022-11-22 08:29:17 -0800

    fix

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -16,7 +16,7 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if ((ft = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)) ==-1)
+	if ((ft = open(argv[2], O_WRONLY |  O_CREAT | O_TRUNC, 0664)) ==-1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
@@ -36,7 +36,7 @@ int main(int argc, char **argv)
 	}
 	if ((close(fd) == -1))
 	{
-		dprintf(2, "Error: Can't close fd %ld\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %ld\n", fd);
 		exit(100);
 	}
 	if ((close(ft) == -1))
