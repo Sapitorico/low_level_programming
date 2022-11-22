0beeeb50b27e1f6b41a690debe8b474b0c74b2c8
Sapitorico <renzoyaques@gmail.com>
2022-11-22 08:46:17 -0800

    fix betty style

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -12,35 +12,30 @@ int main(int argc, char **argv)
 	char buff[BUFF_SIZE];
 
 	if (argc != 3)
-	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	}
-	if ((fd = open(argv[1], O_RDONLY)) == -1)
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
-	if ((ft = open(argv[2], O_WRONLY |  O_CREAT | O_TRUNC, 0664)) == -1)
-	{
+	ft = open(argv[2], O_WRONLY |  O_CREAT | O_TRUNC, 0664);
+	if (ft == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	}
 	while ((rd = read(fd, buff, BUFF_SIZE)))
 	{
 		if (rd == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
 		}
-		if ((wr = write(ft, buff, rd)) == -1)
-		{
+		wr = write(ft, buff, rd);
+		if (wr == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-		}
 	}
 	if ((close(fd) == -1))
-	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %ld\n", fd), exit(100);
-	}
 	if ((close(ft) == -1))
-	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %ld\n", ft), exit(100);
-	}
 	return (0);
 }
