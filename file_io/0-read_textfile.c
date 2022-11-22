b1b7fa48cb7f7feed7a1bcdf6c4bc59d979c4db5
Sapitorico <renzoyaques@gmail.com>
2022-11-22 06:02:23 -0800

    dw

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -12,19 +12,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer = NULL;
 
 	if (!filename)
-		return (0);
+		return (-1);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (0);
+		return (-1);
 	buffer = malloc(sizeof(char *) * letters);
 	if (!buffer)
-		return (0);
+		return (-1);
 	len  = read(fd, buffer, letters);
 	if (len == -1)
-		return (0);
+		return (-1);
 	wr = write(STDOUT_FILENO, buffer, len);
 	if (wr == -1)
-		return (0);
+		return (-1);
 	close(fd);
 	return (wr);
 }
