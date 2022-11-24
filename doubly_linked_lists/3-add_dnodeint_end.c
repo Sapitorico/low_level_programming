824911c5c715f048e5c768f1134e0ab24006d069
Sapitorico <renzoyaques@gmail.com>
2022-11-24 05:17:13 -0800

    adds a new node at the end

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include "2-add_dnodeint.c"
+/**
+ * add_dnodeint_end - add a node at the end
+ * @head: head of the list
+ * @n: integer type
+ *
+ * Return: end node
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *end = NULL, *node = *head;
+
+	if (!node)
+	{
+		*head = add_dnodeint(head, n);
+		return (*head);
+	}
+	while (node->next)
+		node = node->next;
+	end = add_dnodeint_end(&(node->next), n);
+	return (end);
+}
