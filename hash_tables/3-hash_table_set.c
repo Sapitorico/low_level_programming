ed2085d71a6b9911de587b275007b22ad562ad84
Sapitorico <renzoyaques@gmail.com>
2022-11-24 11:51:20 -0800

    adds an element to the hash table.

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+/**
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node = NULL;
+	unsigned long int hash_table;
+	char *new_value = NULL;
+
+	if (!key || !ht)
+		return (0);
+	new_value = strdup(value);
+	if (!new_value)
+		return (0);
+	hash_table = key_index((const unsigned char *) key, ht->size);
+	new_node = ht->array[hash_table];
+	if (new_node != NULL && strcmp(new_node->key, key) == 0)
+	{
+		free(new_node->value);
+		new_node->value = new_value;
+	}
+	else
+	{
+		new_node = malloc(sizeof(hash_node_t));
+		if (!new_node)
+			return (0);
+		new_node->key = strdup(key);
+		if (!new_node->key)
+			return (0);
+		new_node->value = new_value;
+		new_node->next = ht->array[hash_table];
+		ht->array[hash_table] = new_node;
+	}
+	return (1);
+}
