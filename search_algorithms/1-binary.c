2c53739b31a60c1d3fe293818cb0f675d16f2020
Sapitorico <renzoyaques@gmail.com>
2023-01-06 13:51:36 -0300

    wqdqwd

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,45 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in a sorted array of integers
+ *
+ * @array: array
+ * @size: size
+ * @value: value
+ * Return: int
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left = 0, right = size - 1, mid;
+	size_t i = 0;
+
+	if (!array)
+		return (-1);
+	printf("Searching in array: ");
+	for (i = 0; i < size; i++)
+		printf("%d ", array[i]);
+	printf("\n");
+	while (left < right)
+	{
+		mid = (left + right) / 2;
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+		{
+			left = mid + 1;
+			printf("Searching in array: ");
+			for (i = left; i <= right; i++)
+				printf("%d ", array[i]);
+			printf("\n");
+		}
+		else
+		{
+			right = mid - 1;
+			printf("Searching in array: ");
+			for (i = left; i <= right; i++)
+				printf("%d ", array[i]);
+			printf("\n");
+		}
+	}
+	return (-1);
+}
