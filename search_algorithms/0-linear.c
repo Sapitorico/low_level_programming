97e1cb2b45ad8ff994cbf451f27887ba1333b1fe
Sapitorico <renzoyaques@gmail.com>
2023-01-06 12:58:55 -0300

    fix

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -12,11 +12,13 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
+	if (!array)
+		return;
 	for (i = 0; i < size -1; i++)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 	}
 	return (-1);
 }
