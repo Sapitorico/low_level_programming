e4858f7573262ebe8c8a052ea3c60aa6f75bd5fc
Sapitorico <renzoyaques@gmail.com>
2023-01-08 22:44:49 -0300

    asadasdas

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -25,7 +25,7 @@ int binary_search(int *array, size_t size, int value)
 			printf(", ");
 	}
 	printf("\n");
-	while (low < high)
+	while (low <= high)
 	{
 		mid = (low + high) / 2;
 		if (array[mid] == value)
