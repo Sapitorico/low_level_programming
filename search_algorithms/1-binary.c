9cb77fae4f49641a8ca2636011b72270df9ce1a8
Sapitorico <renzoyaques@gmail.com>
2023-01-08 23:01:47 -0300

    add ,

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -22,7 +22,12 @@ int binary_search(int *array, size_t size, int value)
 		middle = (left + right) / 2;
 		printf("Searching in array: ");
 		for (i = left; i <= right; i++)
-			printf("%d ", array[i]);
+		{
+			if (i < right)
+				printf("%d, ", array[i]);
+			else
+				printf("%d ", array[i]);
+		}
 		printf("\n");
 		if (array[middle] == value)
 			return (middle);
